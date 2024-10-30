8918fe7bf41a15f6d66fdd9727c3a5ab9e9ac27b
Laiba Aleem <laiba4most@gmail.com>
2024-10-30 16:48:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// let str="saylani mass it traning"
+// console.log(str)
+// // str=str.slice(0,7)
+// console.log(str);
+// str=str.slice(16)
+// // console.log(str);
+// // str=str.slice(8,15)
+//  console.log(str)
+//  console.log(str.slice(16));
+    // let day=prompt("what day is today!")
+    // day=day.toLowerCase()
+   
+    // if(day=="monday"||day=="tuesday"||day=="wednesday"||day=="thursday"||day=="saturday" ){
+    //     console.log("working day");
+    // }
+    // else if(day=="friday"||day=="sunday"){
+    //     console.log("Holiday");
+    // }
+                                                          
+// let month=prompt("enter is month name ")
+// let charsinmonth=month.length;
+// //console.log(charsinmonth);
+// if(charsinmonth>3){
+//     document.write(month.slice(0,3))
+// }
+//  
